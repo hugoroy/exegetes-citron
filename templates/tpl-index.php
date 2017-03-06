e04016544de048c5a1bdcb54007dd5b46d8f274f
Hugo Roy <hugo@hugoroy.eu>
2017-03-06 22:20:48 +0100

    index - à repenser

--- templates/tpl-index.php
+++ templates/tpl-index.php
@@ -3,43 +3,40 @@ include_once 'templates/tpl-main-top.php';
 ?>
 
 
-<h1>Presse à Citron</h1>
+    <h1>Presse à Citron</h1>
+
+    <p class="bievenue">
+	Bienvenue ! Le presse citron est encore en version beta. <strong>Sélectionne un projet à gauche</strong> ou bien
+        <a href="editer.php?type=project">ajoute un nouveau projet</a> !
+    </p>
+
+    <div class="éditer" id="ajouter">
+	<form>
+	<div class="form-group">
+	    <label for="projetNom">Nom du projet:</label>
+	    <input type="text" class="form-control" id="projetNom" name="projetNom" placeholder="Projet de mémoire">
+	</div>
+	<!--div class="form-group">
+	    <label for="projetId">Identifiant:</label>
+	    <input type="text" class="form-control" id="projetId" name="projetId" placeholder="(automatiquement suggéré en fonction du nom)">
+	</div-->
+	<div class="form-group">
+	    <label for="projetDossier">Dossier: <a href="/ajout-dossier.html">(Nouveau dossier ?)</a></label>
+	    <select name="dossier" id="dossiers" multiple="" class="form-control input-lg select2 select2-offscreen" tabindex="-1">
+		<option value="intveld">In 't Veld</option>
+		<option value="abroretention">Abrogation de la rétention des données</option>
+		<option value="tes">Fichier TES</option>
+		<option value="orangefail">#OrangeFail</option>
+		<option value="prishield">Privacy Shield</option>
+	    </select>
+	</div>
+	<div class="actions"> 
+	    <button type="submit" class="disabled btn btn-lg">Ajouter</button>
+	</div>
+	</form>
+    </div>
 
 
-<h2>Liste des projets</h2>
-<p class="bienvenue">
-	Choisis un projet ou bien <a href="editer.php?type=project">ajoute un nouveau projet</a>.
-</p>
-<ul class='projects'>
-	<?php
-	foreach ($projects as $o) {
-		echo "
-			<li>
-				<a href='voir.php?type=project&id={$o['rowid']}'>{$o['name']}</a>
-				-
-				<i><span>{$o['dossier']}</span></i>
-			</div>
-		";
-	}
-	?>
-</ul>
-
-<h2>Liste des dossiers</h2>
-<p class="bienvenue">
-	Choisis un dossier ou bien <a href="editer.php?type=dossier">ajoute un nouveau dossier</a>.
-</p>
-<ul class='dossiers'>
-	<?php
-	foreach ($dossiers as $o) {
-		echo "
-			<li>
-				<a href='voir.php?type=dossier&id={$o['rowid']}'>{$o['name']}</a>
-			</div>
-		";
-	}
-	?>
-</ul>
-	
 
 <?php
 include_once 'templates/tpl-main-bottom.php';
