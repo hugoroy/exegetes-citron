9e6b66fad0c8498dc83630abc4bd3f2c21eac731
Hugo Roy <hugoroy@users.noreply.github.com>
2017-03-07 18:38:48 +0100

    Instructions de bienvenue

--- templates/tpl-index.php
+++ templates/tpl-index.php
@@ -7,10 +7,10 @@ include_once 'templates/tpl-main-top.php';
 
     <p class="bievenue">
 	Bienvenue ! Le presse citron est encore en version beta. <strong>Sélectionne un projet à gauche</strong> ou bien
-        <a href="editer.php?type=project">ajoute un nouveau projet</a> !
+        <a href="editer.php?type=project">ajoute un nouveau projet</a> (ou <a href="editer.php?type=dossier">un nouveau dossier</a>) !
     </p>
 
-    <div class="éditer" id="ajouter">
+    <div class="éditer hidden" id="ajouter">
 	<form>
 	<div class="form-group">
 	    <label for="projetNom">Nom du projet:</label>
@@ -21,7 +21,7 @@ include_once 'templates/tpl-main-top.php';
 	    <input type="text" class="form-control" id="projetId" name="projetId" placeholder="(automatiquement suggéré en fonction du nom)">
 	</div-->
 	<div class="form-group">
-	    <label for="projetDossier">Dossier: <a href="/ajout-dossier.html">(Nouveau dossier ?)</a></label>
+	    <label for="projetDossier">Dossier: <a href="/editer.php?type=dossier">(Nouveau dossier ?)</a></label>
 	    <select name="dossier" id="dossiers" multiple="" class="form-control input-lg select2 select2-offscreen" tabindex="-1">
 		<option value="intveld">In 't Veld</option>
 		<option value="abroretention">Abrogation de la rétention des données</option>
