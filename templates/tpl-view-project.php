c4e578d03f84698990831f6f4f594f3fcc397482
Hugo Roy <hugo@hugoroy.eu>
2017-09-16 16:52:59 +0200

    vue plus pratique d'un projet

--- templates/tpl-view-project.php
+++ templates/tpl-view-project.php
@@ -4,15 +4,21 @@ include_once 'templates/tpl-main-top.php';
 
 <body id="view-project-<?php echo $o['rowid']; ?>" class="page-view-project">
 
-    <h1 class="app titre"><?php echo $o['name']; ?></h1>
+    <h1 class="app titre">
+      <a href="/voir.php?type=dossier&id=<?php echo $o['dossier_id']; ?>" class="dossier" id="<?php echo $o['dossier']; ?>"><?php echo $o['dossier']; ?></a> / 
+    <?php echo $o['name']; ?>
+	    <a href="editer.php?type=project&id=<?php echo $o['rowid']; ?>" class="modifier">
+		    Éditer
+	    </a>
+    </h1>
 
-    <div class="métadonnées">
+    <div class="métadonnées hide">
       <span class="heading">Dossier</span>
       <a href="/voir.php?type=dossier&id=<?php echo $o['dossier_id']; ?>" class="dossier" id="<?php echo $o['dossier']; ?>"><?php echo $o['dossier']; ?></a>
     </div>
 
 
-    <div class="éditer">
+    <div class="voir">
 	<form action="/bartender/shake" method="get">
 	<div class="form-group hidden">
 	    <label for="projetId">Identifiant:</label>
@@ -20,13 +26,15 @@ include_once 'templates/tpl-main-top.php';
 	</div>
 	<div class="form-group">
 	    <label for="projetPadPrincipal">Pad principal:</label>
-	    <input type="url" class="form-control" id="projetPadPrincipal" name="projetPadPrincipal" placeholder="https://pad.exegetes.eu.org/" value="<?php echo $o['main_pad']; ?>">
+	    <input type="url" class="form-control hidden" id="projetPadPrincipal" name="projetPadPrincipal" placeholder="https://pad.exegetes.eu.org/" value="<?php echo $o['main_pad']; ?>">
+        <a href="<?php echo $o['main_pad']; ?>"><?php echo $o['main_pad']; ?></a>
 	</div>
 	<div class="form-group">
 	    <label for="projetPadGarde">Page de garde:</label>
-	    <input type="url" class="form-control" id="projetPadGarde" name="projetPadGarde" placeholder="https://pad.exegetes.eu.org/" value="<?php echo $o['cover_pad']; ?>">
+	    <input type="url" class="form-control hidden" id="projetPadGarde" name="projetPadGarde" placeholder="https://pad.exegetes.eu.org/" value="<?php echo $o['cover_pad']; ?>">
+        <a href="<?php echo $o['cover_pad']; ?>"><?php echo $o['cover_pad']; ?></a>
 	</div>
-	<div class="form-group">
+	<div class="form-group hidden">
 	    <label for="projetPadAutre1">Autre pad: <a href="#">Ajouter</a></label>
 	    <input type="url" class="form-control hidden" id="projetPadAutre1" name="projetPadAutre1" placeholder="https://pad.exegetes.eu.org/">
 	</div>
@@ -48,9 +56,6 @@ include_once 'templates/tpl-main-top.php';
 
 	<div class="actions"> 
 	    <button type="submit" class="bouton presse">Presser</button>
-	    <a href="editer.php?type=project&id=<?php echo $o['rowid']; ?>" type="submit" class="bouton">
-		    Éditer
-	    </a>
 	</div>
 	</form>
     </div>
